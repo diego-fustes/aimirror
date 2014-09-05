4bf8a37ab825e1cd69f91b62d893ef1095a01cea
diego-fustes <diegofustesfic@gmail.com>
2014-09-05 17:38:38 +0200

    Adding tests folder
    A mock test has been added

---
+++ src/test/java/GarmentsTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by diego.fustes on 05/09/2014.
+ */
+public class GarmentsTest {
+
+    @BeforeClass
+    public static void init() throws Exception {
+
+    }
+
+    @Test
+    public void testSomething() {
+        Assert.assertTrue(true);
+    }
+
+
+}
